b634ce2de1ff5b4d46bdc870f96bcef2cfeb3a7a
Kaifas <cs.stormindustries@outlook.com>
2021-09-24 08:22:10 -0500

    testing cargo test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Rect {
+    width: u32,
+    height: u32,
+}
+impl Rect {
+    fn can_hold(&self, var: u32) -> bool {
+        var > self.width
+    }
+}
+#[test]
+fn larger() {
+    let r = Rect {
+        width: 30,
+        height: 30,
+    };
+    assert!(r.can_hold(20));
+}
