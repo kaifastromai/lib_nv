854307e8f002eb8fbde56778dc927c9dc93b51c1
Joshua Storm <cs.stormindustries@outlook.com>
2022-02-07 14:26:05 -0600

    Remove build files

--- build/CMakeFiles/test_cargo.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_cargo"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_cargo.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jstrom/dev/lib_nv
-# Build directory: /home/jstrom/dev/lib_nv/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
