6f20205ff38647a4986c13e28853f61a20bc1c1d
Kaifas <cs.stormindustries@outlook.com>
2022-02-07 08:40:55 -0600

    Start building FFI

--- nv_api/src/lib.rs
+++ nv_api/src/lib.rs
@@ -7,8 +7,3 @@ pub struct Event {
     pub id: u128,
 }
 
-#[no_mangle]
-pub extern "C" fn mir_new() -> *mut Mir {
-    let mir = Box::new(mir::Mir::new());
-    Box::into_raw(mir)
-}
--- nv_api/src/mir/mod.rs
+++ nv_api/src/mir/mod.rs
@@ -13,7 +13,8 @@ pub struct Mir {
     action_queue: VecDeque<Action>,
 }
 impl Mir {
-    pub fn new() -> Self {
+    #[no_mangle]
+    pub extern "C" fn create() -> Self {
         Mir {
             em: EntityManager::new(),
             proj: Project::new_empty(),
@@ -21,15 +22,10 @@ impl Mir {
             action_queue: VecDeque::new(),
         }
     }
-    pub fn say_hello(&self) {
+    pub extern "C" fn say_hello(&self) {
         println!("Hello from Mir");
     }
 }
-
-//An event is recieved from the clientside and describes something that needs to be done. Mir is responsible for executing the event.
-// An event is different from an action in that an event is not undoable and comes directly from the client.
-//Mir takes an event, converts it to an action if appropriate, and then executes the action.
-//A single event can be translated to multiple actions.
 
 pub trait Returnable {}
 pub trait Event {
